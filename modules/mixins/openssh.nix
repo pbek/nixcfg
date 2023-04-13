0c0e5e346ee584bdf1e04831acbbec55dc9536b0
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-13 16:37:31 +0200

    openssh: allow X11 forwarding

--- modules/mixins/openssh.nix
+++ modules/mixins/openssh.nix
@@ -3,6 +3,7 @@
   services.openssh = {
     enable = true;
     passwordAuthentication = false;
+    forwardX11 = true;
     openFirewall = lib.mkForce true;
     permitRootLogin = lib.mkForce "no";
   };
