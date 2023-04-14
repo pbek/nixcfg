517959167d57c96afddd5e0dfd4ab74f75ae96db
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-14 14:39:46 +0200

    ssh: add authorizedKeys

--- modules/mixins/openssh.nix
+++ modules/mixins/openssh.nix
@@ -6,5 +6,6 @@
     forwardX11 = true;
     openFirewall = lib.mkForce true;
     permitRootLogin = lib.mkForce "no";
+    openssh.authorizedKeys.keys = ["sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBFDWxqigrXdCx7mX/yvBpHJf2JIab9HIrjof+sCbn0cOr/NySAirjE7tWxkZJPBrUs/8wSgn/rFO742O+NkOXTYAAAAEc3NoOg== omega@i7work"];
   };
 }
