26af2a4c383d4e3a0dfb18def177c8fe183d8aae
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-22 22:29:12 +0200

    jupiter: fix nvidia settings

--- hosts/jupiter/configuration.nix
+++ hosts/jupiter/configuration.nix
@@ -53,5 +53,15 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
   # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
-#  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.modesetting.enable = true;
+
+  hardware.nvidia.prime = {
+    sync.enable = true;
+
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:3:0:0";
+
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+  };
 }
