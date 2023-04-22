df7c60cd428bd97699637afa0f521195fa7ec6bd
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-22 12:13:26 +0200

    add: host pluto and setup doc

--- README.md
+++ README.md
@@ -4,6 +4,49 @@
 
 NixOS config
 
+## Setup
+
+```bash
+# set hostname
+export HOSTNAME=pluto
+
+# setup nixos configuration
+cd /etc
+sudo mv nixos nixos.bak
+sudo nix-shell -p git --run 'git clone https://github.com/pbek/nixcfg.git nixos'
+sudo chown omega:users nixos -R
+cd nixos
+mkdir hosts/${HOSTNAME}
+cp ../nixos.bak/* hosts/${HOSTNAME}
+ln -s hosts/${HOSTNAME}/configuration.nix
+ln -s hosts/${HOSTNAME}/hardware-configuration.nix
+
+# edit configuration.nix
+kate . &
+
+# switch to unstable channel and upgrade
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos && sudo nixos-rebuild switch --upgrade
+
+# switch git remote to ssh
+nix-shell -p git --run 'git remote set-url origin git@github.com:pbek/nixcfg.git'
+
+# look at network load?
+nix-shell -p nload --run 'nload enp0s25'
+
+# login at ther computer and start the restic mount and restore
+
+# take over tmux session at local system to watch restore
+tmux new-session -A -s main
+
+# after backup restore reboot computer
+sudo reboot
+
+# run backup script
+```
+
+In the end commit changes to https://github.com/pbek/nixcfg.
+
+
 ## Commands
 
 ```bash
---
+++ hosts/pluto/configuration.nix
@@ -0,0 +1,63 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ../../modules/mixins/users.nix
+    ../../modules/mixins/desktop-common.nix
+    ../../modules/mixins/jetbrains.nix
+    ../../modules/mixins/openssh.nix
+#    ../../modules/editor/nvim.nix
+    # this brought me an infinite recursion
+#    mixins-openssh
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  # Enable grub cryptodisk
+  boot.loader.grub.enableCryptodisk=true;
+
+  boot.initrd.luks.devices."luks-ca98a921-4340-44ee-980f-587a1fd9dff0".keyFile = "/crypto_keyfile.bin";
+  networking.hostName = "pluto"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+
+
+  # Extract
+
+  # https://nixos.wiki/wiki/VirtualBox
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "omega" ];
+
+  # https://nixos.wiki/wiki/steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  # https://nixos.wiki/wiki/nvidia
+#  services.xserver.videoDrivers = [ "nvidia" ];
+#  hardware.opengl.enable = true;
+}
