3b6685782722830bb2b0fcffe72779a3281cc5af
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-22 13:45:53 +0200

    pluto: try nvidia-drm.modeset

--- hosts/pluto/configuration.nix
+++ hosts/pluto/configuration.nix
@@ -58,6 +58,8 @@
   };
 
   # https://nixos.wiki/wiki/nvidia
-#  services.xserver.videoDrivers = [ "nvidia" ];
-#  hardware.opengl.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+  # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
+  hardware.nvidia.modesetting.enable = true;
 }
