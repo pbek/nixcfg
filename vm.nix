13b1a2b29086068c6a21661aa3e4aefadf96a522
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-25 17:03:49 +0200

    vm: add more doc and packages

--- vm.nix
+++ vm.nix
@@ -1,13 +1,25 @@
+#
 # NixOS Test VM
 #
-# nixos-rebuild -I nixos-config=./vm.nix build-vm
+# https://nixos.wiki/wiki/Cheatsheet#Building_a_service_as_a_VM_.28for_testing.29
+# https://nixos.wiki/wiki/NixOS:nixos-rebuild_build-vm
+# https://www.mankier.com/8/nixos-rebuild
+#
+# build in vm
+# > nixos-rebuild -I nixos-config=./vm.nix build-vm
+#
+# build pr in vm
+# > nixos-rebuild -I nixos-config=./vm.nix -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/pull/223593/head.tar.gz build-vm
+# > ./result/bin/run-*-vm
+#
 { lib, config, ... }:
 {
     imports =
     [ # Include the results of the hardware scan.
     ./modules/mixins/users.nix
     ./modules/mixins/desktop-common.nix
-#    ./modules/mixins/jetbrains.nix
+    ./modules/mixins/jetbrains.nix
+    ./modules/mixins/openssh.nix
 #    ./modules/editor/nvim.nix
     # this brought me an infinite recursion
 #    mixins-openssh
