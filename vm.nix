e2b48d8ce432a5fb9e0c7f91d9908af42a17bec8
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-25 15:33:22 +0200

    add: vm.nix to test config in VMs

---
+++ vm.nix
@@ -0,0 +1,17 @@
+# vm.nix
+{ lib, config, ... }:
+{
+    imports =
+    [ # Include the results of the hardware scan.
+    ./modules/mixins/users.nix
+    ./modules/mixins/desktop-common.nix
+#    ./modules/mixins/jetbrains.nix
+#    ./modules/editor/nvim.nix
+    # this brought me an infinite recursion
+#    mixins-openssh
+    ];
+
+  services.tor.enable = true;
+  users.users.root.initialPassword = "root";
+}
+
