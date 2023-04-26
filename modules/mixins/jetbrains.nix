d5a00567b58ba08a520bd9c48dfd631f061ff21a
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-26 08:56:02 +0200

    jetbrains: use tools from PR with plugins

--- modules/mixins/jetbrains.nix
+++ modules/mixins/jetbrains.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }:
-{
+{ config, pkgs, inputs, ... }:
+
+let
+  pr = import
+    (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/pull/223593/head.tar.gz)
+    # reuse the current configuration
+    { config = config.nixpkgs.config; };
+in {
   environment.systemPackages = with pkgs; [
-    jetbrains.phpstorm
-    # (jetbrains.plugins.addPlugins jetbrains.phpstorm [ "github-copilot" ])
-    jetbrains.clion
-    # (jetbrains.plugins.addPlugins jetbrains.clion [ "github-copilot" ])
+    # jetbrains.phpstorm
+    (pr.jetbrains.plugins.addPlugins pr.jetbrains.phpstorm [ "github-copilot" ])
+    # jetbrains.clion
+    (pr.jetbrains.plugins.addPlugins pr.jetbrains.clion [ "github-copilot" ])
     jetbrains.goland
+    # (pr.jetbrains.plugins.addPlugins pr.jetbrains.goland [ "github-copilot" ])
   ];
 }
