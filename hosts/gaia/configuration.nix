3e97c5f066a305fea10bec9b66ff622fe3d55ff1
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-27 08:35:52 +0200

    gaia: add host config

---
+++ hosts/gaia/configuration.nix
@@ -0,0 +1,72 @@
+# Office Work PC
+
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ../../modules/mixins/users.nix
+    ../../modules/mixins/desktop-common.nix
+    ../../modules/mixins/jetbrains.nix
+    ../../modules/mixins/openssh.nix
+    ../../modules/mixins/virt-manager.nix
+#    ../../modules/editor/nvim.nix
+    # this brought me an infinite recursion
+#    mixins-openssh
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  networking.hostName = "gaia"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+
+  environment.systemPackages = with pkgs; [
+    gparted
+  ];
+
+  # Extract
+
+  # https://nixos.wiki/wiki/VirtualBox
+  # virtualisation.virtualbox.host.enable = true;
+  # users.extraGroups.vboxusers.members = [ "omega" ];
+  # virtualisation.virtualbox.host.enableExtensionPack = true;
+
+  # https://nixos.wiki/wiki/steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  # https://nixos.wiki/wiki/nvidia
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
+  # The NVIDIA GeForce GTX 760 GPU needs the NVIDIA 470.xx Legacy drivers
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+
+  # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
+  # hardware.nvidia.modesetting.enable = true;
+}
