490d361d01e71edcf7d71fb859bde4322987e284
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-28 17:58:40 +0200

    jetbrains: add desktop entries with nix-shell

--- modules/mixins/jetbrains.nix
+++ modules/mixins/jetbrains.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, xdg, ... }:
 
 let
   prForJBPlugins = import
@@ -19,4 +19,28 @@ in {
     prForJBPlugins.jetbrains.goland
     # (prForJBPlugins.jetbrains.plugins.addPlugins prForJBPlugins.jetbrains.goland [ "github-copilot" ])
   ];
+
+  home-manager.users.omega = {
+    xdg.desktopEntries = {
+      clion-nix-shell = {
+        name = "CLion with nix-shell";
+        genericName = "C/C++ IDE. New. Intelligent. Cross-platform";
+        comment = "Test Enhancing productivity for every C and C++ developer on Linux, macOS and Windows.";
+        icon = "${prForJBPlugins.jetbrains.clion}/share/pixmaps/clion.svg";
+        exec = "nix-shell /etc/nixos/shells/qt5.nix --run clion";
+        terminal = false;
+        categories = [ "Development" ];
+      };
+
+      phpstorm-nix-shell = {
+        name = "PhpStorm with nix-shell";
+        genericName = "Professional IDE for Web and PHP developers";
+        comment = "PhpStorm provides an editor for PHP, HTML and JavaScript with on-the-fly code analysis, error prevention and automated refactorings for PHP and JavaScript code.";
+        icon = "${prForJBPlugins.jetbrains.phpstorm}/share/pixmaps/phpstorm.svg";
+        exec = "nix-shell /etc/nixos/shells/webdev.nix --run phpstorm";
+        terminal = false;
+        categories = [ "Development" ];
+      };
+    };
+  };
 }
