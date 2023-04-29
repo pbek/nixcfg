303361d10725317b0d98705f49d5d8643716caf8
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-29 11:29:52 +0200

    venus: try to detect OSes

--- hosts/venus/configuration.nix
+++ hosts/venus/configuration.nix
@@ -24,6 +24,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub.useOSProber = true;
 
   # Setup keyfile
   boot.initrd.secrets = {
