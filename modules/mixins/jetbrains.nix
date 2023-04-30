46cbe8db92aa97cf6ab8183bf9c3188b3ffc1966
Patrizio Bekerle <patrizio@bekerle.com>
2023-04-30 12:23:43 +0200

    jetbrains: use new PR force push (breaks clion plugins)

--- modules/mixins/jetbrains.nix
+++ modules/mixins/jetbrains.nix
@@ -3,8 +3,8 @@
 let
   prForJBPlugins = import
     (builtins.fetchTarball {
-      url = https://github.com/NixOS/nixpkgs/archive/pull/223593/head.tar.gz;
-      sha256 = "sha256:1wc70sq3nzk58kp9bbzlw2f57df02d7jclf5dhqc2sr0jk3psiaf";
+      url = https://github.com/NixOS/nixpkgs/archive/62ba85c05e275112a365083ebde5317f0042be6d.tar.gz;
+      sha256 = "sha256:0gbxwm1zabnwjmsiql1ywmh32ls44wfkd2f770p9vldwjgfnja7v";
     })
     {
       config = config.nixpkgs.config;
@@ -16,8 +16,8 @@ in {
     (prForJBPlugins.jetbrains.plugins.addPlugins prForJBPlugins.jetbrains.phpstorm [ "github-copilot" ])
     # jetbrains.clion
     (prForJBPlugins.jetbrains.plugins.addPlugins prForJBPlugins.jetbrains.clion [ "github-copilot" ])
-    prForJBPlugins.jetbrains.goland
-    # (prForJBPlugins.jetbrains.plugins.addPlugins prForJBPlugins.jetbrains.goland [ "github-copilot" ])
+    # prForJBPlugins.jetbrains.goland
+    (prForJBPlugins.jetbrains.plugins.addPlugins prForJBPlugins.jetbrains.goland [ "github-copilot" ])
   ];
 
   home-manager.users.omega = {
