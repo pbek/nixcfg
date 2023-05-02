034902f24f01736e6409fef488320840f1b8a6fa
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-02 12:57:19 +0200

    desktop-common: add LC_ALL

--- modules/mixins/desktop-common.nix
+++ modules/mixins/desktop-common.nix
@@ -26,6 +26,7 @@ in
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_AT.UTF-8";
+    LC_ALL = "de_AT.UTF-8";
     LC_IDENTIFICATION = "de_AT.UTF-8";
     LC_MEASUREMENT = "de_AT.UTF-8";
     LC_MONETARY = "de_AT.UTF-8";
