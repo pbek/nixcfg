30dc372f235ff9c7751b9b956da02d95b0ac847e
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-08 14:02:16 +0200

    desktop-common: disable LC_ALL and LC_CTYPE

--- modules/mixins/desktop-common.nix
+++ modules/mixins/desktop-common.nix
@@ -26,8 +26,8 @@ in
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_AT.UTF-8";
-    LC_ALL = "en_US.UTF-8";
-    LC_CTYPE = "en_US.UTF-8";
+    # LC_ALL = "en_US.UTF-8";
+    # LC_CTYPE = "en_US.UTF-8";
     LC_IDENTIFICATION = "de_AT.UTF-8";
     LC_MEASUREMENT = "de_AT.UTF-8";
     LC_MONETARY = "de_AT.UTF-8";
