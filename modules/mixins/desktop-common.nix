effb0330383d498e342b6f9b5b73145b34b5095a
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-08 10:00:34 +0200

    desktop-common: add cache public key

--- modules/mixins/desktop-common.nix
+++ modules/mixins/desktop-common.nix
@@ -107,13 +107,14 @@ in
   nix = {
     settings = {
       substituters = [
-        "https://nix-cache.qownnotes.org/main"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
+        "https://nix-cache.qownnotes.org/main"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "main:WYsIaF+ItMNE9Xt976bIGKSKp9jaaVeTzYlfqQqpP28="
       ];
     };
   };
