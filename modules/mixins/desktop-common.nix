c669772c169669dd69a8e7fbb71089b1ff354b0e
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-13 10:10:56 +0200

    starship: make more design changes

--- modules/mixins/desktop-common.nix
+++ modules/mixins/desktop-common.nix
@@ -415,9 +415,18 @@ in
           success_symbol = "[➜](bold green)";
           error_symbol = "[✗](bold red)";
         };
+        hostname = {
+          ssh_only = false;
+          # leaving out the "in" at the end because there is not $directory 
+          # setting afterwards any more
+          format = "[$ssh_symbol$hostname]($style) ";
+        };
 
         # Move the directory to the second line
+        # https://starship.rs/config/#default-prompt-format
         format = "$all$directory$character";
+
+        # format = "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery$time$status$os$container$shell$directory$character";
 
         # https://github.com/catppuccin/starship
         # https://github.com/catppuccin/starship/blob/main/palettes/mocha.toml
