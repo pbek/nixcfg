e98c3811e9f79c8c15d03ad9f472cf4195bd09aa
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-13 09:45:59 +0200

    pluto: enable virtual box

--- hosts/pluto/configuration.nix
+++ hosts/pluto/configuration.nix
@@ -46,8 +46,8 @@
   # Extract
 
   # https://nixos.wiki/wiki/VirtualBox
-  # virtualisation.virtualbox.host.enable = true;
-  # users.extraGroups.vboxusers.members = [ "omega" ];
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "omega" ];
   # virtualisation.virtualbox.host.enableExtensionPack = true;
 
   # https://nixos.wiki/wiki/steam
