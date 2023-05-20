29ad7dfa27ca8e14019662ce193a33bc3c1f48b7
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-20 12:01:01 +0200

    agenix: set nixpkgs-review.env

--- modules/mixins/users.nix
+++ modules/mixins/users.nix
@@ -39,4 +39,15 @@
     # Yubikey public key
     openssh.authorizedKeys.keys = ["sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBFDWxqigrXdCx7mX/yvBpHJf2JIab9HIrjof+sCbn0cOr/NySAirjE7tWxkZJPBrUs/8wSgn/rFO742O+NkOXTYAAAAEc3NoOg== omega@i7work"];
   };
+
+  # Add nixpkgs-review environment
+  age.secrets = {
+    nixpkgs-review = {
+      file = ../../secrets/nixpkgs-review.age;
+      path = "/home/omega/.secrets/nixpkgs-review.env";
+      owner = "omega";
+      group = "users";
+      mode = "600";
+    };
+  };
 }
