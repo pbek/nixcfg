753efddd0cb5e4a2489edb9e2f773895a252ed78
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-24 14:36:10 +0200

    vscode: add working config

---
+++ docker/shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = [
+      pkgs.x11docker
+      pkgs.xorg.libxcvt
+      pkgs.catatonit
+      pkgs.wmctrl
+    ];
+}
+
