440477492f2afb6f6afc350e04fa37938e0b78be
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-25 16:57:31 +0200

    noseyparker: do cleanup

--- apps/noseyparker/default-0.13.0.nix
+++ apps/noseyparker/default-0.13.0.nix
@@ -12,22 +12,19 @@
 rustPlatform.buildRustPackage rec {
   pname = "noseyparker";
   version = "0.13.0";
-#   version = "0.12.0";
 
   src = fetchFromGitHub {
     owner = "praetorian-inc";
     repo = "noseyparker";
     rev = "v${version}";
    hash = "sha256-ApL9wqTktxOjLD7IVoaVStvkccJNnpxntNTNBGGxl+M=";
-    # hash = "sha256-qop6KjTFPQ5o1kPEVPP0AfDfr8w/JP3YmC+sb5OUbDY=";
   };
 
   cargoHash = "sha256-Om9uaCL+hC9r82fbEykCwtsdF1OTW0WE108Yv5g677Y=";
-#   cargoHash = "sha256-ZtoJO/R11qTFYAE6G7AVCpnYZ3JGrxtVSXvCm0W8DAA=";
 
   postPatch = ''
     # disabledTests (network, failing)
-    rm tests/* -Rf
+    # rm tests/* -Rf
   '';
 
   nativeBuildInputs = [
