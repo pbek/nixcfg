2233fd03bd172ff44fa7f4e57fe546de610dddae
Patrizio Bekerle <patrizio@bekerle.com>
2023-05-25 14:51:23 +0200

    doc: mention TU

--- hosts/eris/configuration.nix
+++ hosts/eris/configuration.nix
@@ -1,4 +1,4 @@
-# HP EliteBook Laptop
+# TU HP EliteBook Laptop
 
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
