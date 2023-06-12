73305722378f8586a5fa028b93076c29cf490c52
Patrizio Bekerle <patrizio@bekerle.com>
2023-06-12 09:50:28 +0200

    desktop-common: remove duplicate packages and move starship to own config

--- modules/mixins/desktop-common.nix
+++ modules/mixins/desktop-common.nix
@@ -1,6 +1,8 @@
 { config, pkgs, inputs, ... }:
-{ imports = [
+{
+  imports = [
     ./git.nix
+    ./starship.nix
   ];
 
   boot.kernel.sysctl = {
@@ -107,13 +109,6 @@
     };
   };
 
-  # https://nixos.wiki/wiki/Fonts
-  fonts.fonts = with pkgs; [
-    fira-code
-    fira-code-symbols
-    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-  ];
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -146,7 +141,6 @@
     jq
     fzf
     chromium
-    topgrade
     filelight
     # Builds qtwebkit from source!
 #    qt5.full
@@ -210,14 +204,12 @@
     keepassxc
     inetutils
     dig
-    nmap
     gnumake
     gcc
     gdb
     cmake
     chromium
     google-chrome
-    wget
     vscode
     yubikey-manager
     pam_u2f
@@ -232,7 +224,6 @@
     ferdium
     topgrade
     ksnip
-    sops
     ncdu
     sniffnet
     wireguard-tools
@@ -342,21 +333,6 @@
       thefuck --alias | source
     '';
 
-    # enable starship prompt in fish shell, enableFishIntegration in the starship config did not work
-    home.file.".config/fish/conf.d/starship.fish".text = ''
-      starship init fish | source
-    '';
-
-    # Enable starship for bash (did not work)
-    # programs.bash.bashrcExtra = ''
-    #   eval "$(starship init bash)"
-    # '';
-
-    # Enable starship for bash
-    home.file.".bash_aliases".text = ''
-      eval "$(starship init bash)"
-    '';
-
     # Set the path to the pia-manual repository and the username and password for the PIA VPN script
     home.file."Scripts/pia.sh" = {
       text = ''
@@ -403,70 +379,6 @@
       enable = true;
       enableFishIntegration = true;
       nix-direnv.enable = true;
-    };
-
-    # enable https://starship.rs
-    programs.starship =
-    let
-      flavour = "mocha"; # One of `latte`, `frappe`, `macchiato`, or `mocha`
-    in {
-      enable = true;
-      enableFishIntegration = true;
-      enableBashIntegration = true;
-
-      # https://starship.rs/config
-      # Lookup icons: https://www.nerdfonts.com/cheat-sheet
-      settings = {
-        # add_newline = false;
-        directory = {
-          fish_style_pwd_dir_length = 3; # The number of characters to use when applying fish shell pwd path logic.
-          truncation_length = 1; # The number of parent folders that the current directory should be truncated to.
-          style = "bold sky";
-        };
-        git_branch = {
-          style = "bold pink";
-        };
-        username = {
-          disabled = false;
-          show_always = true;
-        };
-        character = {
-          success_symbol = "[➜](bold green)";
-          error_symbol = "[✗](bold red)";
-        };
-        hostname = {
-          ssh_only = false;
-          # leaving out the "in" at the end because there is not $directory 
-          # setting afterwards any more
-          format = "[$ssh_symbol$hostname]($style) ";
-        };
-        # https://starship.rs/config/#shell
-        shell = {
-          disabled = false;
-          # format = "\\[[$indicator]($style)\\] ";
-          style = "gray";
-          fish_indicator = "󰈺";
-          # bash_indicator = "b";
-        };
-        status.disabled = false;
-
-        # Move the directory to the second line
-        # https://starship.rs/config/#default-prompt-format
-        format = "$all$directory$status$character";
-
-        # format = "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery$time$status$os$container$shell$directory$character";
-
-        # https://github.com/catppuccin/starship
-        # https://github.com/catppuccin/starship/blob/main/palettes/mocha.toml
-        palette = "catppuccin_${flavour}";
-      } // builtins.fromTOML (builtins.readFile
-        (pkgs.fetchFromGitHub
-          {
-            owner = "catppuccin";
-            repo = "starship";
-            rev = "3e3e54410c3189053f4da7a7043261361a1ed1bc";
-            sha256 = "sha256-soEBVlq3ULeiZFAdQYMRFuswIIhI9bclIU8WXjxd7oY=";
-          } + /palettes/${flavour}.toml));
     };
   };
 
---
+++ modules/mixins/starship.nix
@@ -0,0 +1,91 @@
+{ config, pkgs, inputs, ... }:
+{
+  # https://nixos.wiki/wiki/Fonts
+  fonts.fonts = with pkgs; [
+    fira-code
+    fira-code-symbols
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+  ];
+
+  # https://rycee.gitlab.io/home-manager/options.html
+  home-manager.users.omega = {
+    # enable starship prompt in fish shell, enableFishIntegration in the starship config did not work
+    home.file.".config/fish/conf.d/starship.fish".text = ''
+      starship init fish | source
+    '';
+
+    # Enable starship for bash (did not work)
+    # programs.bash.bashrcExtra = ''
+    #   eval "$(starship init bash)"
+    # '';
+
+    # Enable starship for bash
+    home.file.".bash_aliases".text = ''
+      eval "$(starship init bash)"
+    '';
+
+    # enable https://starship.rs
+    programs.starship =
+    let
+      flavour = "mocha"; # One of `latte`, `frappe`, `macchiato`, or `mocha`
+    in {
+      enable = true;
+      enableFishIntegration = true;
+      enableBashIntegration = true;
+
+      # https://starship.rs/config
+      # Lookup icons: https://www.nerdfonts.com/cheat-sheet
+      settings = {
+        # add_newline = false;
+        directory = {
+          fish_style_pwd_dir_length = 3; # The number of characters to use when applying fish shell pwd path logic.
+          truncation_length = 1; # The number of parent folders that the current directory should be truncated to.
+          style = "bold sky";
+        };
+        git_branch = {
+          style = "bold pink";
+        };
+        username = {
+          disabled = false;
+          show_always = true;
+        };
+        character = {
+          success_symbol = "[?](bold green)";
+          error_symbol = "[?](bold red)";
+        };
+        hostname = {
+          ssh_only = false;
+          # leaving out the "in" at the end because there is not $directory
+          # setting afterwards any more
+          format = "[$ssh_symbol$hostname]($style) ";
+        };
+        # https://starship.rs/config/#shell
+        shell = {
+          disabled = false;
+          # format = "\\[[$indicator]($style)\\] ";
+          style = "gray";
+          fish_indicator = "?";
+          # bash_indicator = "b";
+        };
+        status.disabled = false;
+
+        # Move the directory to the second line
+        # https://starship.rs/config/#default-prompt-format
+        format = "$all$directory$status$character";
+
+        # format = "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery$time$status$os$container$shell$directory$character";
+
+        # https://github.com/catppuccin/starship
+        # https://github.com/catppuccin/starship/blob/main/palettes/mocha.toml
+        palette = "catppuccin_${flavour}";
+      } // builtins.fromTOML (builtins.readFile
+        (pkgs.fetchFromGitHub
+          {
+            owner = "catppuccin";
+            repo = "starship";
+            rev = "3e3e54410c3189053f4da7a7043261361a1ed1bc";
+            sha256 = "sha256-soEBVlq3ULeiZFAdQYMRFuswIIhI9bclIU8WXjxd7oY=";
+          } + /palettes/${flavour}.toml));
+    };
+  };
+}
