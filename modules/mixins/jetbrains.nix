bd2934532639cf297728d935c1ae2ec87b8eac4c
Patrizio Bekerle <patrizio@bekerle.com>
2023-07-02 13:23:31 +0200

    jetbrains: use jetbrains suit with plugins PR from nixpkgs unstable (finally!)

--- modules/mixins/jetbrains.nix
+++ modules/mixins/jetbrains.nix
@@ -1,18 +1,7 @@
 { config, pkgs, inputs, xdg, ... }:
 
-# https://github.com/NixOS/nixpkgs/pull/240521
-let
-  prForJBPlugins = import
-    (builtins.fetchTarball {
-      url = https://github.com/NixOS/nixpkgs/archive/424cc823107a1d9c5a924b3cf9556629b03748a7.tar.gz;
-      sha256 = "sha256:0a3p2dd6jggnbisqcrxrbifx7cm3kwly49mg5cqsz7qs7a7icw63";
-    })
-    {
-      config = config.nixpkgs.config;
-      localSystem = { system = "x86_64-linux"; };
-    };
-in {
-  environment.systemPackages = with prForJBPlugins; [
+{
+  environment.systemPackages = with pkgs; [
     (jetbrains.plugins.addPlugins jetbrains.phpstorm [ "github-copilot" ])
     # jetbrains.clion
     (jetbrains.plugins.addPlugins jetbrains.clion [ "github-copilot" ])
@@ -26,7 +15,7 @@ in {
         name = "CLion with nix-shell";
         genericName = "C/C++ IDE. New. Intelligent. Cross-platform";
         comment = "Test Enhancing productivity for every C and C++ developer on Linux, macOS and Windows.";
-        icon = "${prForJBPlugins.jetbrains.clion}/share/pixmaps/clion.svg";
+        icon = "${pkgs.jetbrains.clion}/share/pixmaps/clion.svg";
         exec = "nix-shell /etc/nixos/shells/qt5.nix --run clion";
         terminal = false;
         categories = [ "Development" ];
@@ -36,7 +25,7 @@ in {
         name = "PhpStorm with nix-shell";
         genericName = "Professional IDE for Web and PHP developers";
         comment = "PhpStorm provides an editor for PHP, HTML and JavaScript with on-the-fly code analysis, error prevention and automated refactorings for PHP and JavaScript code.";
-        icon = "${prForJBPlugins.jetbrains.phpstorm}/share/pixmaps/phpstorm.svg";
+        icon = "${pkgs.jetbrains.phpstorm}/share/pixmaps/phpstorm.svg";
         exec = "nix-shell /etc/nixos/shells/webdev.nix --run phpstorm";
         terminal = false;
         categories = [ "Development" ];
@@ -46,7 +35,7 @@ in {
         name = "Goland with nix-shell";
         genericName = "Up and Coming Go IDE";
         comment = "Goland is the codename for a new commercial IDE by JetBrains aimed at providing an ergonomic environment for Go development. The new IDE extends the IntelliJ platform with the coding assistance and tool integrations specific for the Go language.";
-        icon = "${prForJBPlugins.jetbrains.goland}/share/pixmaps/goland.svg";
+        icon = "${pkgs.jetbrains.goland}/share/pixmaps/goland.svg";
         exec = "nix-shell -p go --run goland";
         terminal = false;
         categories = [ "Development" ];
