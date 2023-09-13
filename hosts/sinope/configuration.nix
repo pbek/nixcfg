830f62f6a9ca7b6ac99512ce3bba2bc33f9debc8
Patrizio Bekerle <patrizio@bekerle.com>
2023-09-13 13:36:19 +0200

    sinope: add multipass

--- hosts/sinope/configuration.nix
+++ hosts/sinope/configuration.nix
@@ -37,6 +37,9 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # For testing https://gitlab.tugraz.at/vpu-private/ansible/
+  virtualisation.multipass.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
