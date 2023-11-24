dc32355b66dfaaab7d42df4d55619f7cdbed40b1
Patrizio Bekerle <patrizio@bekerle.com>
2023-11-24 10:49:04 +0100

    php: switch to php83

--- files/shells/webdev.nix
+++ files/shells/webdev.nix
@@ -3,23 +3,23 @@
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs; [
       nodejs-18_x
-      php81
-      php81Extensions.ldap
-      php81Extensions.gd
-      php81Extensions.mysqli
-      php81Extensions.soap
-      php81Extensions.ldap
-      php81Extensions.mbstring
-      php81Extensions.xml
-      php81Extensions.intl
-      php81Extensions.apcu
-      php81Extensions.curl
-      php81Extensions.readline
-      php81Extensions.zip
-      php81Extensions.redis
-      php81Extensions.gmp
-      php81Extensions.sqlite3
-      php81Extensions.xdebug
-      php81Packages.composer
+      php83
+      php83Extensions.ldap
+      php83Extensions.gd
+      php83Extensions.mysqli
+      php83Extensions.soap
+      php83Extensions.ldap
+      php83Extensions.mbstring
+      php83Extensions.xml
+      php83Extensions.intl
+      php83Extensions.apcu
+      php83Extensions.curl
+      php83Extensions.readline
+      php83Extensions.zip
+      php83Extensions.redis
+      php83Extensions.gmp
+      php83Extensions.sqlite3
+      php83Extensions.xdebug
+      php83Packages.composer
     ];
 }
