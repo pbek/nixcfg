46b7ff09032a0a9c2143ab9f8b08780b291c5bad
Patrizio Bekerle <patrizio@bekerle.com>
2023-12-10 14:21:02 +0100

    qownnotes: 23.12.0 -> 23.12.1

--- apps/qownnotes/default.nix
+++ apps/qownnotes/default.nix
@@ -18,14 +18,14 @@
 let
   pname = "qownnotes";
   appname = "QOwnNotes";
-  version = "23.12.0";
+  version = "23.12.1";
 in
 stdenv.mkDerivation {
   inherit pname appname version;
 
   src = fetchurl {
     url = "https://github.com/pbek/QOwnNotes/releases/download/v${version}/qownnotes-${version}.tar.xz";
-    hash = "sha256-GZIdsWqwLrzctDPXwHdlK3c4SxwZB/v9+jUimo+IaOQ=";
+    hash = "sha256-9KP1/9oTq2ujc863oV5QmBR6wscj1rFiZXatZtrG2V8=";
   };
 
   nativeBuildInputs = [
