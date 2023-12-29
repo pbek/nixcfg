baadbbd3ef0574e4ffcab1e39dcb9344b1477d02
Patrizio Bekerle <patrizio@bekerle.com>
2023-12-29 12:51:18 +0100

    espanso: add GitHub triggers

--- modules/mixins/espanso.nix
+++ modules/mixins/espanso.nix
@@ -6,6 +6,7 @@
       enable = true;
       matches = {
         base = {
+          # https://espanso.org/docs/matches/basics/
           matches = [
             {
               trigger = ":sg";
@@ -26,7 +27,7 @@
             {
               trigger = ":date";
               replace = "{{mydate}}";
-              vars =  [
+              vars = [
                 {
                   name = "mydate";
                   type = "date";
@@ -35,6 +36,31 @@
                   };
                 }
               ];
+            }
+            {
+              trigger = ":ghrel";
+              replace = "There now is a new release, could you please test it and report if it works for you?\n";
+            }
+            {
+              triggers = [ ":ghtest" ":ghtst" ];
+              replace = "Great, thank you for testing!\n";
+            }
+            {
+              trigger = ":ghscr";
+              replace = ''
+                Can you please mention the authors of the script, like advised in the issue template?
+                You will find the authors in the *Script repository*.
+                Don't forget to use the `@` symbol to mention them.
+              '';
+            }
+            {
+              triggers = [ ":ghrep" ":ghlog" ];
+              replace = ''
+                Thank you for reporting.
+                Can you please post the output from the *Debug settings* in the settings dialog (see issue template)?
+                And is there any useful log output if you enable the log panel (see Window / Panels)?
+                Please make sure you also enable debug logging in the log panel by right-clicking on the panel and selecting `Debug` in `Options`.
+              '';
             }
           ];
         };
