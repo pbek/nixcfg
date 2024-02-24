e66f7e08f99f9d7a0f487e9080f0ee209b3352ec
Patrizio Bekerle <patrizio@bekerle.com>
2024-02-24 12:18:38 +0100

    home01: disable intel microcode

--- hosts/home01/hardware-configuration.nix
+++ hosts/home01/hardware-configuration.nix
@@ -49,5 +49,5 @@
   # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+#  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
