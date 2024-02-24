7b15a43d53b535e45e5d6d1d577bf0d81eb0c79e
Patrizio Bekerle <patrizio@bekerle.com>
2024-02-24 12:49:26 +0100

    server-local: remove redundant setting

--- modules/mixins/server-local.nix
+++ modules/mixins/server-local.nix
@@ -4,11 +4,6 @@
     ./server-common.nix
   ];
 
-  # https://mynixos.com/options/services.openssh
-  services.openssh = {
-    listenAddresses = [ { addr = "0.0.0.0"; port = 22; } ];
-  };
-
   # Firewall
   # https://nixos.wiki/wiki/Firewall
   networking.firewall = {
