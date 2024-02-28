ae0e096453ec514a88f60d3640bc92b7831a976f
Patrizio Bekerle <patrizio@bekerle.com>
2024-02-28 09:19:52 +0100

    doc: fix host naming

--- hosts/moobox01/vm.nix
+++ hosts/moobox01/vm.nix
@@ -1,12 +1,12 @@
 #
-# NixOS Netcup02 Test VM
+# NixOS moobox01 Test VM
 #
 # https://nixos.wiki/wiki/Cheatsheet#Building_a_service_as_a_VM_.28for_testing.29
 # https://nixos.wiki/wiki/NixOS:nixos-rebuild_build-vm
 # https://www.mankier.com/8/nixos-rebuild
 #
 # build in vm
-# > nixos-rebuild --flake .#vm-netcup02 build-vm
+# > nixos-rebuild --flake .#vm-moobox01 build-vm
 #
 { lib, config, ... }:
 {
