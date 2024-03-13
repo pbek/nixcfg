eea595f2e08dfddd4961bad8de8de12d60125d3f
Patrizio Bekerle <patrizio@bekerle.com>
2024-03-13 20:28:41 +0100

    desktop-common-plasma6: migrate setting

--- modules/mixins/desktop-common-plasma6.nix
+++ modules/mixins/desktop-common-plasma6.nix
@@ -1,6 +1,6 @@
 { config, pkgs, inputs, ... }:
 {
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # GTK themes are not applied in Wayland applications / Window Decorations missing / Cursor looks different
   # https://nixos.wiki/wiki/KDE#GTK_themes_are_not_applied_in_Wayland_applications_.2F_Window_Decorations_missing_.2F_Cursor_looks_different
