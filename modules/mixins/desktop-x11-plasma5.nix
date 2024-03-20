ab1bf7f652e662abbca0afe91e835aef84f195c0
Patrizio Bekerle <patrizio@bekerle.com>
2024-03-20 09:27:45 +0100

    desktop-x11-plasma5: add now missing pinentryPackage setting

--- modules/mixins/desktop-x11-plasma5.nix
+++ modules/mixins/desktop-x11-plasma5.nix
@@ -4,4 +4,7 @@
     ./desktop-common.nix
     ./desktop-common-plasma5.nix
   ];
+
+  # You seem to need to set the default pinentry if you ar using X11 with Plasma5
+  programs.gnupg.agent.pinentryPackage = pkgs.pinentry-qt;
 }
