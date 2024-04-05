25011a607b02bb4b3794a24e56d7f7dbad350e17
Patrizio Bekerle <patrizio@bekerle.com>
2024-04-05 20:53:19 +0200

    ally: use plasmax11 as default session

--- hosts/ally/configuration.nix
+++ hosts/ally/configuration.nix
@@ -25,6 +25,7 @@
   };
 
   services.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.defaultSession = "plasmax11";
 
   # GTK themes are not applied in Wayland applications / Window Decorations missing / Cursor looks different
   # https://nixos.wiki/wiki/KDE#GTK_themes_are_not_applied_in_Wayland_applications_.2F_Window_Decorations_missing_.2F_Cursor_looks_different
