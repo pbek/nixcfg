29ef2d99bb34b93f686099301fab5547a01b96f4
Patrizio Bekerle <patrizio@bekerle.com>
2024-06-22 12:57:24 +0200

    miniserver24: add username description
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/miniserver24/configuration.nix
+++ hosts/miniserver24/configuration.nix
@@ -37,6 +37,7 @@
   security.sudo.wheelNeedsPassword = false;
 
   users.users.${username} = {
+    description = "Markus";
     openssh.authorizedKeys.keys = [
       # Markus public key
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGIQIkx1H1iVXWYKnHkxQsS7tGsZq3SoHxlVccd+kroMC/DhC4MWwVnJInWwDpo/bz7LiLuh+1Bmq04PswD78EiHVVQ+O7Ckk32heWrywD2vufihukhKRTy5zl6uodb5+oa8PBholTnw09d3M0gbsVKfLEi4NDlgPJiiQsIU00ct/y42nI0s1wXhYn/Oudfqh0yRfGvv2DZowN+XGkxQQ5LSCBYYabBK/W9imvqrxizttw02h2/u3knXcsUpOEhcWJYHHn/0mw33tl6a093bT2IfFPFb3LE2KxUjVqwIYz8jou8cb0F/1+QJVKtqOVLMvDBMqyXAhCkvwtEz13KEyt"
