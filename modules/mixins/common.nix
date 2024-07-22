36b6818478eed2813293ee258afd12a45543fcd9
Patrizio Bekerle <patrizio@bekerle.com>
2024-07-22 10:06:09 +0200

    common: increase nh clean times
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/mixins/common.nix
+++ modules/mixins/common.nix
@@ -46,7 +46,7 @@
     nh = {
       enable = true;
       clean.enable = true;
-      clean.extraArgs = "--keep-since 4d --keep 3";
+      clean.extraArgs = "--keep-since 14d --keep 4";
     };
   };
 
