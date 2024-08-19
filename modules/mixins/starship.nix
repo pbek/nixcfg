10f73416e3dbf171a5219bc3b02c9fdb57227793
Patrizio Bekerle <patrizio@bekerle.com>
2024-08-19 09:11:06 +0200

    flake: update and fix starship config file path
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/mixins/starship.nix
+++ modules/mixins/starship.nix
@@ -64,9 +64,9 @@
         # format = "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery$time$status$os$container$shell$directory$character";
 
         # https://github.com/catppuccin/starship
-        # https://github.com/catppuccin/starship/blob/main/palettes/mocha.toml
+        # https://github.com/catppuccin/starship/blob/main/themes/mocha.toml
         palette = "catppuccin_${flavour}";
-      } // builtins.fromTOML (builtins.readFile"${inputs.catppuccin}/palettes/${flavour}.toml");
+      } // builtins.fromTOML (builtins.readFile"${inputs.catppuccin}/themes/${flavour}.toml");
     };
   };
 }
