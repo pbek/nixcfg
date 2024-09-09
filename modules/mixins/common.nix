acbe8af90fdc7341eff56eb87a87ca16feb470f1
Patrizio Bekerle <patrizio@bekerle.com>
2024-09-09 13:54:13 +0200

    common: add gitpls alias
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/mixins/common.nix
+++ modules/mixins/common.nix
@@ -13,6 +13,7 @@
         gitps = "git push";
         gitplr = "git pull --rec";
         gitpl = "git pull && git submodule update --init";
+        gitpls = "git pull";
         gita = "git add -A";
         gits = "git status";
         gitd = "git diff";
