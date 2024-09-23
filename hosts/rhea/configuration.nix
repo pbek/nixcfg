5edeed0bac7a4d762872f9e5e7d6da925d46b7c7
Patrizio Bekerle <patrizio@bekerle.com>
2024-09-23 07:28:10 +0200

    rhea: restart network and docker after suspend
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/rhea/configuration.nix
+++ hosts/rhea/configuration.nix
@@ -36,4 +36,12 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+
+  # Restart network and docker after suspend
+  # I had issues with KDE Plasma detecting that there is network after suspend
+  powerManagement.resumeCommands = ''
+    nmcli n off
+    nmcli n on
+    systemctl restart docker
+  '';
 }
