a6cd255867ff8939c70d8ec0ceaadaec77846931
Patrizio Bekerle <patrizio@bekerle.com>
2024-10-10 10:08:42 +0200

    espanso: enable workaround again, since espanso stopped working again
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/mixins/espanso-latest.nix
+++ modules/mixins/espanso-latest.nix
@@ -4,9 +4,9 @@
   boot.kernelModules = [ "uinput" ];
 
   # Get around permission denied error on /dev/uinput
-#  services.udev.extraRules = ''
-#    KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput", GROUP="input", MODE="0660"
-#  '';
+  services.udev.extraRules = ''
+    KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput", GROUP="input", MODE="0660"
+  '';
 
   home-manager.users.${username} = {
     # https://mynixos.com/home-manager/options/services.espanso
