ac12776e216c03b73be2760e4b508acb84ac1059
Patrizio Bekerle <patrizio@bekerle.com>
2024-10-17 20:18:40 +0200

    NixOS/nixpkgs#348845 fix: fall back on stable overlay for calibre
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/gaia/configuration.nix
+++ hosts/gaia/configuration.nix
@@ -44,7 +44,7 @@
   services.printing.drivers = [ pkgs.splix ];
 
   environment.systemPackages = with pkgs; [
-    calibre
+    stable.calibre
     arduino-ide
     g810-led # Logitech G213 keyboard
     kdePackages.kdialog
