aeda351dcd4aa9bb68145e2e47f09fa2ad4d042c
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-04 09:45:12 +0100

    NixOS/nixpkgs#353119 fix: use stable yazi
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/mixins/common.nix
+++ modules/mixins/common.nix
@@ -164,6 +164,8 @@
     procs # ps "replacement"
     just # command runner like make
     neosay # send messages to matrix room
+    # Use stable yazi temporarily, because of https://github.com/NixOS/nixpkgs/issues/353119
+    stable.yazi # terminal file manager
   ];
 
   # Do garbage collection
@@ -223,7 +225,9 @@
       # Blazing fast terminal file manager written in Rust
       # https://github.com/sxyazi/yazi
       yazi = {
-        enable = true;
+        # Disable yazi temporarily, because of https://github.com/NixOS/nixpkgs/issues/353119 and
+        # https://github.com/NixOS/nixpkgs/issues/353119#issuecomment-2453521926 caused an error
+#        enable = true;
         enableFishIntegration = true;
         enableBashIntegration = true;
       };
