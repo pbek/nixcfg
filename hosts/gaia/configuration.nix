7f55437740c1142ff6b31cad5662b449563b3612
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-04 20:20:55 +0100

    NixOS/nixpkgs#352598 fix: use stable blender
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/gaia/configuration.nix
+++ hosts/gaia/configuration.nix
@@ -51,7 +51,7 @@
     bitscope.meter
     bitscope.dso
     bitscope.logic
-    blender
+    stable.blender
     # Temporarily disabled for: sip-4.19.25 not supported for interpreter python3.12
 #    cura
     go-passbolt-cli
