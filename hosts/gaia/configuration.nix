34d7d34a8888a1d62bc47a679fd6d28e3f50e218
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-13 10:59:45 +0100

    flake: update flake for blender
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/gaia/configuration.nix
+++ hosts/gaia/configuration.nix
@@ -48,7 +48,7 @@
     bitscope.meter
     bitscope.dso
     bitscope.logic
-    stable.blender
+    blender
     # Temporarily disabled for: sip-4.19.25 not supported for interpreter python3.12
     # Using stable.blender-hip doesn't work because of: Cannot mix incompatible Qt library (5.15.15) with this library (5.15.14)
 #    cura
