9133aadfa2c9150b0767f8b418d1d9fa551bd902
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-14 14:35:25 +0100

    caliban: disable not working line in install script
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/caliban/install.sh
+++ hosts/caliban/install.sh
@@ -4,6 +4,6 @@ set -e
 
 export HOST=caliban
 sudo nix --experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode disko ./hosts/${HOST}/disk-config.zfs.nix
-sudo nix --experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --flake .#${HOST} --write-efi-entries
+#sudo nix --experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --flake .#${HOST} --write-efi-entries
 sudo nixos-install --flake .#${HOST}
 
