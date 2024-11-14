ea8ad223e25fc557f8c29a23b79ef17bee69f57f
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-14 13:32:03 +0100

    caliban: add install script
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

---
+++ hosts/caliban/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+export HOST=caliban
+sudo nix --experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode disko ./hosts/${HOST}/disk-config.zfs.nix
+sudo nix --experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --flake .#${HOST} --write-efi-entries
+sudo nixos-install --flake .#${HOST}
+
