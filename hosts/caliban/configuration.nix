2f106a16a36b048ce07a963c1c47e1231d25f883
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-15 14:34:51 +0100

    caliban: use sanoid for zfs snapshots
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/caliban/configuration.nix
+++ hosts/caliban/configuration.nix
@@ -43,6 +43,25 @@
     '';
   };
 
+  # Add the sanoid service to take snapshots of the ZFS datasets
+  services.sanoid = {
+    enable = true;
+    templates = {
+      hourly = {
+        autoprune = true;
+        autosnap = true;
+        daily = 7;
+        hourly = 24;
+        monthly = 0;
+      };
+    };
+    datasets = {
+      "calroot/encrypted/home" = {
+        useTemplate = ["hourly"];
+      };
+    };
+  };
+
   networking = {
     hostId = "dccada02";  # needed for ZFS
     hostName = "caliban";
