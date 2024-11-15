58042d2805e2f05e2e5ec3e87d538f999bc8b54d
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-15 13:39:08 +0100

    caliban: add additional commands that didn't work
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/caliban/install.sh
+++ hosts/caliban/install.sh
@@ -5,5 +5,7 @@ set -e
 export HOST=caliban
 sudo nix --experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode disko ./hosts/${HOST}/disk-config.zfs.nix
 #sudo nix --experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --flake .#${HOST} --write-efi-entries
+#sudo nix --experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --flake .#${HOST} --write-efi-boot-entries --disk main /dev/nvme1n1
+#sudo nix run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake .#${HOST} --disk disk1 /dev/nvme1n1
 sudo nixos-install --flake .#${HOST}
 
