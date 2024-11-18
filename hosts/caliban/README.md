361149a3f5e02c5bfffb1367f17b846451c29ca2
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-18 13:11:47 +0100

    caliban-store-cache: update host name
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/caliban/README.md
+++ hosts/caliban/README.md
@@ -8,7 +8,7 @@ Also see [Binary Cache](https://nixos.wiki/wiki/Binary_Cache).
 
 ```bash
 cd /etc
-sudo nix-store --generate-binary-cache-key caliban.netbird.cloud cache-priv-key.pem cache-pub-key.pem
+sudo nix-store --generate-binary-cache-key caliban-1.netbird.cloud cache-priv-key.pem cache-pub-key.pem
 sudo chmod 600 cache-priv-key.pem
 cat cache-pub-key.pem
 
--- modules/mixins/caliban-store-cache.nix
+++ modules/mixins/caliban-store-cache.nix
@@ -4,10 +4,10 @@ _:
     settings = {
       substituters = [
         # Local caliban nix binary cache
-        "http://caliban.netbird.cloud:5000"
+        "http://caliban-1.netbird.cloud:5000"
       ];
       trusted-public-keys = [
-        "caliban.netbird.cloud:DqNB0rEKs3/A69mLvTL8tbyqmOZ8SY8OFZsSHkwvEVU="
+        "caliban-1.netbird.cloud:DqNB0rEKs3/A69mLvTL8tbyqmOZ8SY8OFZsSHkwvEVU="
       ];
     };
   };
