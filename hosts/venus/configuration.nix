cb8cd333d0e1f3bc0301e9e7e2774a624fb0ab4c
Patrizio Bekerle <patrizio@bekerle.com>
2024-11-22 21:08:36 +0100

    venus: enable protontricks for steam
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/venus/configuration.nix
+++ hosts/venus/configuration.nix
@@ -79,6 +79,7 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    protontricks.enable = true; # Protontricks is a simple wrapper that does winetricks things for Proton enabled games
   };
 
   # Try to use the latest kernel
