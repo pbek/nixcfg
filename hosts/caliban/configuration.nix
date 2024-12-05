0a3f3ed38b673b6aec88cb59eb08ecbf415a8179
Patrizio Bekerle <patrizio@bekerle.com>
2024-12-05 10:34:37 +0100

    caliban: try kernel 6.11 and unstable zfs
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/caliban/configuration.nix
+++ hosts/caliban/configuration.nix
@@ -121,6 +121,10 @@
   # lts: 6.6
   #  boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  # ZFS (even unstable) is marked broken in kernel 6.12, so we stick to 6.11 and the unstable ZFS package
+  boot.zfs.package = pkgs.zfs_unstable;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_11;
+
   # Enable hardware accelerated graphics drivers
   hardware.graphics.enable = true;
 
