1823459804177d7ca24b8361ec238169e77b51fa
Patrizio Bekerle <patrizio@bekerle.com>
2025-01-06 07:26:25 +0100

    pluto: use latest kernel
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/pluto/configuration.nix
+++ hosts/pluto/configuration.nix
@@ -27,6 +27,8 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  # Use latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "pluto"; # Define your hostname.
 
