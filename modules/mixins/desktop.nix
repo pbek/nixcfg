2958013cd9647919968468105e30382865d2ec08
Patrizio Bekerle <patrizio@bekerle.com>
2025-01-30 14:07:02 +0100

    desktop: remove obsolete code
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/mixins/desktop.nix
+++ modules/mixins/desktop.nix
@@ -5,54 +5,9 @@
   lib,
   ...
 }:
-let
-  #  cfg = config.services.hokage;
-  waylandSupport = config.services.hokage.waylandSupport;
-  #  usePlasma6 = config.services.hokage.usePlasma6;
-  #  useEspanso = config.services.hokage.useEspanso;
-  #  userLogin = config.services.hokage.userLogin;
-  #  useEspansoPlasma6 = usePlasma6 && useEspanso;
-
-in
-#  waylandSupport = true;
-#  usePlasma6 = true;
 {
-  imports =
-    [ ]
-    #    lib.optional (!waylandSupport && usePlasma6) ./desktop-x11.nix
-    #    ++ lib.optional (waylandSupport && usePlasma6) ./desktop-wayland.nix
-    #    ++ lib.optional (!waylandSupport && !usePlasma6) ./desktop-x11-plasma5.nix
-    ++ [
-      # Other imports here
-      ./desktop-common.nix
-    ];
-
-  #  environment.systemPackages = [] ++ lib.optional (!waylandSupport) [ pkgs.xorg.xkill ];
-
-  #  boot.kernelModules = if useEspansoPlasma6 then [ "uinput" ] else [];
-  #
-  #  services.udev.extraRules = if useEspansoPlasma6 then ''
-  #    KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput", GROUP="input", MODE="0660"
-  #  '' else "";
-  #
-  #  home-manager.users.${userLogin} = if useEspansoPlasma6 then {
-  #    services.espanso = {
-  #      package = if waylandSupport then pkgs.espanso-wayland else pkgs.espanso;
-  #      #      package = (pkgs.callPackage ../../apps/espanso/espanso.nix { }).override {
-  #      #        inherit waylandSupport;
-  #      #      };
-  #    };
-  #  } else {};
-
-  #  config = lib.mkMerge [
-  #    (lib.mkIf cfg.usePlasma6 {
-  #      services.displayManager.defaultSession = "plasma";
-  #    })
-  #
-  #    (lib.mkIf useEspansoPlasma6 {
-  #      services.udev.extraRules = ''
-  #        KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput", GROUP="input", MODE="0660"
-  #      '';
-  #    })
-  #  ];
+  imports = [
+    # Other imports here
+    ./desktop-common.nix
+  ];
 }
