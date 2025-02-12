562f797a03e6341aedf22847bd8a083865ad26e5
Patrizio Bekerle <patrizio@bekerle.com>
2025-02-12 09:27:30 +0100

    flake: update to fix blender build issue

--- hosts/gaia/configuration.nix
+++ hosts/gaia/configuration.nix
@@ -57,7 +57,7 @@ in
     #    (pkgs.callPackage ../../apps/bitscope/packages.nix { }).meter
     #    (pkgs.callPackage ../../apps/bitscope/packages.nix { }).dso
     #    (pkgs.callPackage ../../apps/bitscope/packages.nix { }).logic
-    stable.blender
+    blender
     # Temporarily disabled for: sip-4.19.25 not supported for interpreter python3.12
     # Using stable.blender-hip doesn't work because of: Cannot mix incompatible Qt library (5.15.15) with this library (5.15.14)
     #    cura
