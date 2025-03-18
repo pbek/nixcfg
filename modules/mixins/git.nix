1054aef0bd2039ba2293eeecddd09d6e4617f04d
Patrizio Bekerle <patrizio@bekerle.com>
2025-03-18 09:31:31 +0100

    git: consolidate settings to extraConfig
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/mixins/git.nix
+++ modules/mixins/git.nix
@@ -22,56 +22,52 @@ in
       ignores = [
         ".idea"
         ".direnv"
+        "result"
       ];
       signing = {
         signByDefault = false;
         key = "E00548D5D6AC812CAAD2AFFA9C42B05E591360DC";
       };
+      extraConfig = {
+          gc = {
+            autoDetach = false;
+          };
+          url = {
+            "ssh://git@github.com/" = {
+              insteadOf = "https://github.com/";
+            };
+            "ssh://git@gitlab.com/" = {
+              insteadOf = "https://gitlab.com/";
+            };
+            "ssh://git@bitbucket.org/" = {
+              insteadOf = "https://bitbucket.org/";
+            };
+            "ssh://git@gitlab.tugraz.at/" = {
+              insteadOf = "https://gitlab.tugraz.at/";
+            };
+          };
+#          gpg = {
+#            program = "gpg";
+#          };
+          pull = {
+            rebase = true;
+          };
+          gui = {
+            pruneduringfetch = true;
+          };
+          smartgit.submodule = {
+            fetchalways = false;
+            update = true;
+            initializenew = true;
+          };
+          push = {
+            recurseSubmodules = "check";
+          };
+          init = {
+            defaultBranch = "main";
+          };
+        };
     };
-
-    # we had that file by default in the past
-    home.file.".gitignore".text = ''
-      .idea
-      .direnv
-      result
-    '';
-
-    # we had that file by default in the past
-    # Migrate to https://search.nixos.org/options?channel=unstable&show=programs.git.config&from=0&size=50&sort=relevance&type=packages&query=programs.git
-    home.file.".gitconfig".text = ''
-      [user]
-        name = ${userNameLong}
-        email = ${userEmail}
-        signingkey = E00548D5D6AC812CAAD2AFFA9C42B05E591360DC
-      [gc]
-        autoDetach = false
-      [url "ssh://git@github.com/"]
-        insteadOf = https://github.com/
-      [url "ssh://git@gitlab.com/"]
-        insteadOf = https://gitlab.com/
-      [url "ssh://git@bitbucket.org/"]
-        insteadOf = https://bitbucket.org/
-      [url "ssh://git@gitlab.tugraz.at/"]
-        insteadOf = https://gitlab.tugraz.at/
-      [core]
-        excludesfile = /home/${userLogin}/.gitignore
-      [commit]
-        gpgsign = false
-      [gpg]
-        program = gpg
-      [pull]
-        rebase = true
-      [gui]
-        pruneduringfetch = true
-      [smartgit "submodule"]
-        fetchalways = false
-        update = true
-        initializenew = true
-      [push]
-        recurseSubmodules = check
-      [init]
-        defaultBranch = main
-    '';
 
     # Turn on signing off for git commits in lazygit
     home.file.".config/lazygit/config.yml".text = ''
