0239deda8d8ce581843e92d92e667697020097d9
Patrizio Bekerle <patrizio@bekerle.com>
2025-03-22 13:27:07 +0100

    git: automatically create git pre-commit hook
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- shell.nix
+++ shell.nix
@@ -8,4 +8,9 @@ pkgs.mkShell {
     gum
     just
   ];
+
+  shellHook = ''
+    # Symlink the pre-commit hook into the .git/hooks directory
+    ln -sf ../../scripts/pre-commit.sh .git/hooks/pre-commit
+  '';
 }
