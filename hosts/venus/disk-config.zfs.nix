d40d42968af735bf09a73231b5c8c8a30543665e
Patrizio Bekerle <patrizio@bekerle.com>
2025-03-27 20:47:56 +0100

    venus: try non-legacy ZFS mounts
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/venus/disk-config.zfs.nix
+++ hosts/venus/disk-config.zfs.nix
@@ -1,32 +1,28 @@
-{ lib, ... }:
 {
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/nvme0n1";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+    disk = {
+      root = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "nofail" ];
+              };
             };
-          };
-          zfs = {
-            size = "100%";
-            content = {
-              type = "zfs";
-              pool = "zroot";
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
             };
           };
         };
@@ -36,52 +32,42 @@
       zroot = {
         type = "zpool";
         rootFsOptions = {
+          mountpoint = "none";
           compression = "zstd";
+          acltype = "posixacl";
+          xattr = "sa";
           "com.sun:auto-snapshot" = "false";
         };
-        postCreateHook = "zfs snapshot zroot@blank";
-
+        options.ashift = "12";
         datasets = {
-          encrypted = {
+          "root" = {
             type = "zfs_fs";
             options = {
-              mountpoint = "none";
               encryption = "aes-256-gcm";
               keyformat = "passphrase";
               keylocation = "file:///tmp/secret.key";
+              # keylocation = "prompt";
             };
+            mountpoint = "/";
             # use this to read the key during boot
             postCreateHook = ''
               zfs set keylocation="prompt" "zroot/$name";
             '';
           };
-          "encrypted/root" = {
+          "root/nix" = {
             type = "zfs_fs";
-            mountpoint = "/";
-            options = {
-              mountpoint = "legacy";
-            };
-          };
-          "encrypted/home" = {
-            type = "zfs_fs";
-            mountpoint = "/home";
-            options = {
-              mountpoint = "legacy";
-            };
-          };
-          "encrypted/nix" = {
-            type = "zfs_fs";
+            options.mountpoint = "/nix";
             mountpoint = "/nix";
-            options = {
-              mountpoint = "legacy";
-            };
           };
-          "encrypted/docker" = {
+          "root/home" = {
             type = "zfs_fs";
+            options.mountpoint = "/home";
+            mountpoint = "/home";
+          };
+          "root/docker" = {
+            type = "zfs_fs";
+            options.mountpoint = "/var/lib/docker/volumes";
             mountpoint = "/var/lib/docker/volumes";
-            options = {
-              mountpoint = "legacy";
-            };
           };
         };
       };
