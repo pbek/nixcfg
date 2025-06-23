a6f373f09b5edb74948e3f7632c8c6ca526b1016
Patrizio Bekerle <patrizio@bekerle.com>
2025-06-23 09:00:24 +0200

    flake: update for clr fix
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/venus/configuration.nix
+++ hosts/venus/configuration.nix
@@ -55,7 +55,7 @@ in
   environment.systemPackages = with pkgs; [
     calibre
     zoom-us
-    stable.blender-hip # Blender with HIP support for AMD GPUs
+    blender-hip # Blender with HIP support for AMD GPUs
     # Temporarily disabled for: sip-4.19.25 not supported for interpreter python3.12
     # Using stable.blender-hip doesn't work because of: Cannot mix incompatible Qt library (5.15.15) with this library (5.15.14)
     #     cura
@@ -92,7 +92,7 @@ in
   #  ];
 
   services.ollama = {
-    # enable = true;
+    enable = true;
     acceleration = "rocm";
     package = pkgs.ollama;
     #    environmentVariables = {
