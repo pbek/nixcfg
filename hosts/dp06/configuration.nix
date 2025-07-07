bd4d71e57fd9ab6de9d6c39b8b09c761c8d3cb2b
Patrizio Bekerle <patrizio@bekerle.com>
2025-07-07 08:36:30 +0200

    dp06: set previous login
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/dp06/configuration.nix
+++ hosts/dp06/configuration.nix
@@ -24,7 +24,8 @@
 
   hokage = {
     hostName = "dp06";
-    userLogin = "vision";
+    # "user" was the old login, we will keep it, to stay out of trouble
+    userLogin = "user";
     userNameLong = "Shiva Pouya";
     userNameShort = "Shiva";
     userEmail = "shiva.pouya@tugraz.at";
