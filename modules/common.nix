01ebb27dbcf378bc2c8fbe7fd9f8b72fd5f7c44b
Patrizio Bekerle <patrizio@bekerle.com>
2025-07-23 14:27:43 +0200

    common: remove yazi shell wrapper, because it's integrated in shell integration
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/common.nix
+++ modules/common.nix
@@ -323,19 +323,6 @@ in
         };
       };
 
-      # Exit Yazi to the current path
-      # https://yazi-rs.github.io/docs/quick-start/#shell-wrapper
-      fish.functions = {
-        yy = ''
-          set tmp (mktemp -t "yazi-cwd.XXXXXX")
-          yazi $argv --cwd-file="$tmp"
-          if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-            builtin cd -- "$cwd"
-          end
-          rm -f -- "$tmp"
-        '';
-      };
-
       # Post-modern editor (like vim)
       helix = {
         enable = true;
