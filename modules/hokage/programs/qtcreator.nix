baa1208fd39494a67dacaae9047bcad77c125eb6
Patrizio Bekerle <patrizio@bekerle.com>
2025-09-23 20:28:46 +0200

    qtcreator: add desktop file with dev packages again
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/hokage/programs/qtcreator.nix
+++ modules/hokage/programs/qtcreator.nix
@@ -30,6 +30,18 @@ in
         qtc = "nix-shell /home/${_userName}/.shells/qt5.nix --run qtcreator";
         qtc6 = "nix-shell /home/${_userName}/.shells/qt6.nix --run qtcreator";
       };
+
+      xdg.desktopEntries = {
+        qtcreator-nix-shell = {
+          name = "Qt Creator with dev packages";
+          genericName = "C++ IDE for developing Qt applications";
+          comment = "";
+          icon = "${pkgs.qtcreator}/share/icons/hicolor/128x128/apps/QtProject-qtcreator.png";
+          exec = "nix-shell /home/${_userName}/.shells/qt6.nix --run qtcreator";
+          terminal = false;
+          categories = [ "Development" ];
+        };
+      };
     });
   };
 }
