8a576e5f90ea1410837486e324cc2dbd0f4fceee
Patrizio Bekerle <patrizio@bekerle.com>
2025-10-04 17:42:05 +0200

    gaming: allow local network game transfer in steam

--- modules/hokage/gaming.nix
+++ modules/hokage/gaming.nix
@@ -27,7 +27,7 @@ in
       steam = {
         enable = true;
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers.
         protontricks.enable = true; # Protontricks is a simple wrapper that does winetricks things for Proton enabled games
       };
 
