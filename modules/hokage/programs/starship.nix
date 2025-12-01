0f03f7a0c245e3696d5f4121863fddeb9b4abdd0
Patrizio Bekerle <patrizio@bekerle.com>
2025-12-01 22:43:13 +0100

    starship: initialze for fish shell only in interactive shells
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/hokage/programs/starship.nix
+++ modules/hokage/programs/starship.nix
@@ -24,7 +24,7 @@ in
       # enable https://starship.rs
       programs.starship = {
         enable = true;
-        enableFishIntegration = true;
+        enableFishIntegration = false; # We manually initialize it below for interactive shells only
 
         # https://starship.rs/config
         # Lookup icons: https://www.nerdfonts.com/cheat-sheet
@@ -80,6 +80,14 @@ in
           # format = "$username$hostname$localip$shlvl$singularity$kubernetes$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$pijul_channel$docker_context$package$c$cmake$cobol$daml$dart$deno$dotnet$elixir$elm$erlang$fennel$golang$guix_shell$haskell$haxe$helm$java$julia$kotlin$gradle$lua$nim$nodejs$ocaml$opa$perl$php$pulumi$purescript$python$raku$rlang$red$ruby$rust$scala$swift$terraform$vlang$vagrant$zig$buf$nix_shell$conda$meson$spack$memory_usage$aws$gcloud$openstack$azure$env_var$crystal$custom$sudo$cmd_duration$line_break$jobs$battery$time$status$os$container$shell$directory$character";
         };
       };
+
+      # Initialize starship only for interactive shells
+      programs.fish.interactiveShellInit = ''
+        # Only initialize starship in interactive mode
+        if status is-interactive
+          starship init fish | source
+        end
+      '';
     });
   };
 }
