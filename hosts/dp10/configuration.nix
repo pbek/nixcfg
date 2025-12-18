ae22a1e05d7764e52cf966e9c4fba48953b873c4
Patrizio Bekerle <patrizio@bekerle.com>
2025-12-18 08:54:04 +0100

    dp10: enable gaming and add authorized key
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/dp10/configuration.nix
+++ hosts/dp10/configuration.nix
@@ -12,9 +12,17 @@
     ../dp06/shiva.nix
   ];
 
+  users.users.user = {
+    openssh.authorizedKeys.keys = [
+      # Shiva Laptop public key
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIArlTbzSLIYPvhCAIJDDIHt0fWKgjWRxlHMkV9269OKL user@dp06"
+    ];
+  };
+
   hokage = {
     hostName = "dp10";
     zfs.hostId = "e8fd4331";
+    gaming.enable = true;
 
     nvidia = {
       enable = true;
