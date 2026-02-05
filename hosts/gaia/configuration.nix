c63dba74acc41abae4617da3354dda931a4e3d07
Patrizio Bekerle <patrizio@bekerle.com>
2026-02-05 08:18:54 +0100

    gaia: enable uncrash
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- hosts/gaia/configuration.nix
+++ hosts/gaia/configuration.nix
@@ -105,6 +105,10 @@ in
   # Increase the console font size for kmscon
   services.kmscon.extraConfig = "font-size = 26";
 
+  services.uncrash = {
+    enable = true;
+  };
+
   # Try to get around being stuck after resume
   # https://chatgpt.com/c/690d9bc5-e334-832b-b459-86b3e0f9250b
   boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
