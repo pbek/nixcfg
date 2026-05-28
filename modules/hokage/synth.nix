dbad49921f248b822f6c63c34727bcd677bddacb
Patrizio Bekerle <patrizio@bekerle.com>
2026-05-28 22:53:32 +0200

    synth: add more tooling
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- modules/hokage/synth.nix
+++ modules/hokage/synth.nix
@@ -18,10 +18,17 @@ in
   };
 
   config = mkIf cfg.enable {
+    services.pipewire.jack.enable = true;
+
     environment.systemPackages = with pkgs; [
       sysex-controls # Configure Akai, Arturia, Korg MIDI devices via SysEx
       surge-xt # Synthesizer
+      jalv-qt # LV2 plugin host with UI support
+      pipewire.jack # PipeWire JACK wrapper such as pw-jack
+      jack-example-tools # JACK tools such as jack_lsp and jack_connect
       lmms # Digital audio workstation with MIDI support
+      carla # Audio plugin host with MIDI support
+      qpwgraph # PipeWire/JACK patchbay for audio and MIDI routing
     ];
   };
 }
