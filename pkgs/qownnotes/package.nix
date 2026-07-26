0ca8bcc8be82d56ac67183fcd1eb5fcd5767a9a3
Patrizio Bekerle <patrizio@bekerle.com>
2026-07-26 19:25:23 +0200

    qownnotes: 26.7.9 -> 26.7.10

--- pkgs/qownnotes/package.nix
+++ pkgs/qownnotes/package.nix
@@ -19,11 +19,11 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "qownnotes";
   appname = "QOwnNotes";
-  version = "26.7.9";
+  version = "26.7.10";
 
   src = fetchurl {
     url = "https://github.com/pbek/QOwnNotes/releases/download/v${finalAttrs.version}/qownnotes-${finalAttrs.version}.tar.xz";
-    hash = "sha256-KOUKM2Yj6iTQbCkXsfiBBs8lP8gyZ8oxwZniauS44zc=";
+    hash = "sha256-fGg2pfoYtaAkFcAoNVSOSxMPiSGWPDFperbE4GkVg8w=";
   };
 
   nativeBuildInputs = [
