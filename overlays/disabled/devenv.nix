cc7b1eaf62f2f996daac51b45ba73575b353288c
Patrizio Bekerle <patrizio@bekerle.com>
2026-07-31 06:59:59 +0200

    flake: update for devenv 2.2.0, remove devenv overlay

--- overlays/disabled/devenv.nix
+++ overlays/disabled/devenv.nix
@@ -1,15 +1,20 @@
 final: prev:
 
 let
-  # devenv 2.0.6
-  # https://github.com/NixOS/nixpkgs/pull/502149
-  nixpkgsPr502149 = prev.fetchFromGitHub {
+  # devenv 2.2.0
+  # https://github.com/NixOS/nixpkgs/pull/546784
+  nixpkgsPr546784 = prev.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "4fd6b31a326472305c2709c1ce7dc7a8ea5b839d";
-    hash = "sha256-Nbq1syxcy6lJEZpgbHdxuDrfLM7C6fiTkoSxJKynLTU=";
+    rev = "b266e7d2fc3562d97a8b5ed91cc914e8dac4a4c1";
+    hash = "sha256-HevLsehJQgz1xpoXK+Gk+fQTIMGZnnrp39J+1O3LvJc=";
   };
+  libghostty-vt =
+    final.callPackage "${nixpkgsPr546784}/pkgs/by-name/li/libghostty-vt/package.nix"
+      { };
 in
 {
-  devenv = final.callPackage "${nixpkgsPr502149}/pkgs/by-name/de/devenv/package.nix" { };
+  devenv = final.callPackage "${nixpkgsPr546784}/pkgs/by-name/de/devenv/package.nix" {
+    inherit libghostty-vt;
+  };
 }
