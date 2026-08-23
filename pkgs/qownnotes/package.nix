bf77724c1d888d1dcfb83887270346576dcaa334
Patrizio Bekerle <patrizio@bekerle.com>
2026-08-23 22:24:29 +0200

    qownnotes: 26.8.7 -> 26.8.8
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- pkgs/qownnotes/package.nix
+++ pkgs/qownnotes/package.nix
@@ -19,11 +19,11 @@
 stdenv.mkDerivation (finalAttrs: {
   pname = "qownnotes";
   appname = "QOwnNotes";
-  version = "26.8.7";
+  version = "26.8.8";
 
   src = fetchurl {
     url = "https://github.com/pbek/QOwnNotes/releases/download/v${finalAttrs.version}/qownnotes-${finalAttrs.version}.tar.xz";
-    hash = "sha256-lsaHVVD5HkvbUOsFZEcbw+KHETV6I736Mzft1Pz2mSM=";
+    hash = "sha256-i4Eu68ZbWk8qK+0D4AqWqpqM/umeAj9ioTfilsS+LTU=";
   };
 
   nativeBuildInputs = [
