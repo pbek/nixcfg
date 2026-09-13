e96f2f97f4fe6f85126a0713290e480660317ee3
Patrizio Bekerle <patrizio@bekerle.com>
2026-09-13 21:07:14 +0200

    flake: update and use upstream devenv 2.3
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- overlays/disabled/devenv.nix
+++ overlays/disabled/devenv.nix
@@ -1,20 +1,15 @@
 final: prev:
 
 let
-  # devenv 2.2.0
-  # https://github.com/NixOS/nixpkgs/pull/546784
-  nixpkgsPr546784 = prev.fetchFromGitHub {
+  # devenv 2.3.0
+  # https://github.com/NixOS/nixpkgs/pull/560896
+  nixpkgsPr560896 = prev.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "b266e7d2fc3562d97a8b5ed91cc914e8dac4a4c1";
-    hash = "sha256-HevLsehJQgz1xpoXK+Gk+fQTIMGZnnrp39J+1O3LvJc=";
+    rev = "89ea88681d92c564085254528206f7587912f0c1";
+    hash = "sha256-6CjA0V0oV53SCXsg9x14+HfqCV2cyQ+D0ZUTs30SKtc=";
   };
-  libghostty-vt =
-    final.callPackage "${nixpkgsPr546784}/pkgs/by-name/li/libghostty-vt/package.nix"
-      { };
 in
 {
-  devenv = final.callPackage "${nixpkgsPr546784}/pkgs/by-name/de/devenv/package.nix" {
-    inherit libghostty-vt;
-  };
+  devenv = final.callPackage "${nixpkgsPr560896}/pkgs/by-name/de/devenv/package.nix" { };
 }
