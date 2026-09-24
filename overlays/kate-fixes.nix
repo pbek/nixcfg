0ac713faed9c921066f7328def2035bce164e1f8
Patrizio Bekerle <patrizio@bekerle.com>
2026-09-24 08:59:33 +0200

    kate: update git hosting patches

--- overlays/kate-fixes.nix
+++ overlays/kate-fixes.nix
@@ -1,7 +1,7 @@
 # Backport Kate fixes and features.
 # https://invent.kde.org/utilities/kate/-/commit/80d0affb9686f866872bd15b9c4ba7f3d418b398
 # https://invent.kde.org/utilities/kate/-/commit/6df9ec678a5558463866ab4a75977cd1f890cc58
-# https://invent.kde.org/utilities/kate/-/merge_requests/2195
+# https://invent.kde.org/utilities/kate/-/merge_requests/2198
 # The OpenLink patch is rebased onto Kate 26.04.3 because the release is
 # missing a test row that was added on master.
 # Drop patches from this overlay once their fixes land in the Kate release
@@ -13,8 +13,7 @@ _final: prev: {
         patches = (old.patches or [ ]) ++ [
           ./kde/kate-openlink-parens.patch
           ./kde/kate-quickopen-folder-match.patch
-          ./kde/kate-git-forge-project-hook.patch
-          ./kde/kate-git-forge-plugin.patch
+          ./kde/kate-git-forge.patch
         ];
       });
     }
